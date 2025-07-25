182bbda4487dc5cb367e682d6a8079ce62b12f2d
Mehsan-Ali <mehsanaliaziz@gmail.com>
2025-07-25 17:52:38 +0500

    Search Query

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common'
+import { EmployeesService } from './employees.service'
+import { Employee } from './employees.entity'
 
 @Controller('employees')
-export class EmployeesController {}
+export class EmployeesController {
+  constructor (private readonly employeesService: EmployeesService) {}
+
+  @Post('create')
+  createEmployee (@Body() body: Partial<Employee>): Promise<Employee> {
+    return this.employeesService.createEmployee(body)
+  }
+
+  @Get()
+  findAllData (): Promise<Employee[]> {
+    return this.employeesService.findAll()
+  }
+
+  @Get(':id')
+  findById (@Param('id') id: number): Promise<Employee> {
+    return this.employeesService.findOne(id)
+  }
+
+  @Put(':id')
+  updateEmp (
+    @Param('id') id: number,
+    @Body() data: Partial<Employee>,
+  ): Promise<Employee> {
+    return this.employeesService.updateEmployee(id, data)
+  }
+
+  @Delete(':id')
+  deleteEmp (@Param('id') id: number) {
+    return this.employeesService.deleteEmployee(id)
+  }
+}
--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Employee } from './employees.entity'
+import { Repository } from 'typeorm'
 
 @Injectable()
-export class EmployeesService {}
+export class EmployeesService {
+  constructor (
+    @InjectRepository(Employee)
+    private employeesRepository: Repository<Employee>,
+  ) {}
+
+  async createEmployee (data: Partial<Employee>): Promise<Employee> {
+    const employee = await this.employeesRepository.create(data)
+    return this.employeesRepository.save(employee)
+  }
+
+  async findAll (): Promise<Employee[]> {
+    const employees = await this.employeesRepository.find()
+    return employees
+  }
+
+  async findOne (id: number): Promise<Employee> {
+    const employee = await this.employeesRepository.findOneBy({ id })
+    if (!employee) {
+      throw new NotFoundException(`Employee with ID ${id} not found`)
+    }
+    return employee
+  }
+
+  async updateEmployee (id:number, data: Partial<Employee>): Promise<Employee> {
+    const employee = await this.employeesRepository.findOneBy({ id })
+    if (!employee) {
+      throw new NotFoundException(`Employee with ID ${id} not found`)
+    }
+    const updatedEmployee = Object.assign(employee, data)
+    return this.employeesRepository.save(updatedEmployee)
+  }
+
+  async deleteEmployee (id:number) : Promise<{ Message: string }> {
+    const deleted = await this.employeesRepository.delete(id)
+    return { Message: `Employee with ID ${id} deleted successfully ${deleted.affected}` }
+  }
+}
